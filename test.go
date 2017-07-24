712b48ce8bb797a6e094ae80399a975e1d2adac5
yang <jorise@qq.com>
2017-07-24 10:19:04 +0800

    coding

--- test.go
+++ test.go
@@ -5,12 +5,14 @@ import (
     _ "github.com/nogio/noggo/core"
     "github.com/nogio/noggo/middler"
     "github.com/nogio/noggo/driver/data-cockroach"
+    "github.com/nogio/noggo/driver/data-postgres"
     _ "./code"
 )
 
 func main() {
 
     noggo.Data.Driver("cockroach", data_cockroach.Driver())
+    noggo.Data.Driver("postgres", data_postgres.Driver())
 
     noggo.Use(middler.HttpLogger())
     noggo.Launch(":8080")
--- test.sql
+++ test.sql
@@ -1,9 +1,20 @@
 
---numbers
+--numbers on cockroach
 CREATE TABLE "test"."numbers" (
 	"id"		    serial NOT NULL PRIMARY KEY,
-	"value"     float NOT NULL UNIQUE,
-	"status"	  string,
+	"value"     float NOT NULL,
+	"status"	  varchar(50),
+	"changed"	  timestamp NOT NULL DEFAULT now(),
+	"created"	  timestamp NOT NULL DEFAULT now()
+);
+
+
+
+--numbers on postgresql
+CREATE TABLE "public"."numbers" (
+	"id"		    serial NOT NULL PRIMARY KEY,
+	"value"     float NOT NULL,
+	"status"	  varchar(50),
 	"changed"	  timestamp NOT NULL DEFAULT now(),
 	"created"	  timestamp NOT NULL DEFAULT now()
 );
